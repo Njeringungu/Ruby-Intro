52eaef6e6b2d588b62c96addaaec9001c2123604
Njeri Ngungu <janetnjerin89@gmail.com>
2022-08-18 23:44:04 +0300

    done

--- intro.rb
+++ intro.rb
@@ -1,24 +1,24 @@
 
-
-# function studentName(){
+require("pry")
+# function studentemail(){
 
 #     console.log("is reading")
 
 # }
 
-# studentName()
+# studentemail()
 
-def student_name(first,last)
+# def student_email(first,last)
 
-puts "my first name is #{first} and my last name is #{last}"
+# puts "my first email is #{first} and my last email is #{last}"
 
-   puts "is reading"
-   puts "is reading"
-   puts "is reading"
+#    puts "is reading"
+#    puts "is reading"
+#    puts "is reading"
 
-end
+# end
 
-student_name("janet", "Njeri")
+# student_email("janet", "Njeri")
 
 # print "enter a value: "
 # first_num = gets.to_i
@@ -28,3 +28,58 @@ student_name("janet", "Njeri")
 # puts first_num.to_s + "+" + sec_num.to_s + "=" +
 # (first_num + sec_num).to_s
 
+# def greet_user email
+
+# if email.include?("@")
+
+#      " #{email} is valid"
+# else 
+#      "enter a valid email"
+
+# end
+# end
+# puts greet_user "njergmail.com"
+
+# email="janet"
+
+# def student(email)
+# if 1 > 2
+#     puts email
+#     else 
+#         puts "you're wrong"
+# end
+# end
+# student (email)
+
+# age = 3
+# unless age > 4
+# puts "no school"    
+# else 
+#     puts " Go to school"
+# end
+
+# grade = "A"
+# did_i_pass = case grade
+# when "A" then
+#      "You are a genius"
+    
+# when "D" then
+#      "Dont tell my mother"
+    
+# else
+#     "I am finished"
+# end
+# puts did_i_pass
+
+
+def greet_user email
+binding.pry
+if email.include?("@")
+
+     " #{email} is valid"
+else 
+     "enter a valid email"
+
+end
+end
+puts greet_user "njeri@gmail.com"
