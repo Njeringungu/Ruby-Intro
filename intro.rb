d2c041880c67934dc64449eabc19319f901fb46d
Njeri Ngungu <janetnjerin89@gmail.com>
2022-08-18 22:20:19 +0300

    initial commit

---
+++ intro.rb
@@ -0,0 +1,30 @@
+
+
+# function studentName(){
+
+#     console.log("is reading")
+
+# }
+
+# studentName()
+
+def student_name(first,last)
+
+puts "my first name is #{first} and my last name is #{last}"
+
+   puts "is reading"
+   puts "is reading"
+   puts "is reading"
+
+end
+
+student_name("janet", "Njeri")
+
+# print "enter a value: "
+# first_num = gets.to_i
+# print "enter another value: "
+# sec_num = gets.to_i
+
+# puts first_num.to_s + "+" + sec_num.to_s + "=" +
+# (first_num + sec_num).to_s
+
