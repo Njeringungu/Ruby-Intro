6eafd02b1ce2f3277c5187ae014e77d4ea768da1
mmulahvictor <mmulahvictor@gmail.com>
2022-08-25 21:47:51 +0300

    minor changes on the home component

--- Twitter-Clone-React/twitter/src/components/Home.js
+++ Twitter-Clone-React/twitter/src/components/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <h2>Home</h2>
+        <FontAwesomeIcon icon="fa-solid fa-stars" />
+      </div>
+    </div>
   )
 }
 
